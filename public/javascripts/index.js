f5d9b7a66e0b605bd2f932398ec4f3f72b5c4dcb
Aprillyn Matalog <april.matalog08@gmail.com>
2019-02-18 13:29:49 +0800

    part 2 commit changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,56 +4,111 @@ var checkList = false;
 var editList = false;
 
 
+//deleting extra characters
+
+	inputOneLength = 20;
+	inputTwoLength = 18;
+
+	$('#one').on('keyup keydown change', function () {
+		if($(this).val().length > inputOneLength){
+
+			val=$(this).val().substr(0,$(this).val().length-1);
+			$(this).val(val);
+		};
+	});
+	
+	$('#two').on('keyup keydown change', function () {
+		if($(this).val().length > inputTwoLength){
+
+			val=$(this).val().substr(0,$(this).val().length-1);
+			$(this).val(val);
+		};
+	});
+	
+	$('#mdOne').on('keyup keydown change', function () {
+		if($(this).val().length > inputOneLength){
+
+			val=$(this).val().substr(0,$(this).val().length-1);
+			$(this).val(val);
+		};
+	});
+	
+	$('#mdTwo').on('keyup keydown change', function () {
+		if($(this).val().length > inputTwoLength){
+
+			val=$(this).val().substr(0,$(this).val().length-1);
+			$(this).val(val);
+		};
+	});
+
+//table1 sort
+
+	const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+	const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+		v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+		)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+		document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+		const table = th.closest('table');
+		Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+			.sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+			.forEach(tr => table.appendChild(tr) );
+	})));
+
+//check button	
+
 	function myJson()
 	{
 		var table = document.getElementById("myTable"), rIndex;
 		var inputOne = $('#one').val();
 		var inputTwo = $('#two').val();
-		var edtButton = 'EDIT';
+		var edtButton = 'EDIT' + '<i class="fa fa-edit" aria-hidden="true"></i>';
+        var deleteBtn = 'Delete' + '<i class="fa fa-trash-o" aria-hidden="true"></i>';
+		var today = new Date();
 		var i;
-
-
-		if ((inputOne == "") || (inputTwo == "") )
+		
+		
+		if ((inputOne == "") || (inputTwo == "") ) 
 		{
 			alert("Please complete all required field!");
 		}
 		else
-		{
+		{  
 			if(csnList.length == 0 || psnList.length == 0)
 			{
-				for (i = 0; i < myData.table5Data.length; i++)
+				for (i = 0; i < myData.table5Data.length; i++) 
 				{
-
+					
 					if((inputOne == myData.table5Data[i].cartridge_sn) && (inputTwo == myData.table5Data[i].printer_sn))
 					{
 						var csnVal = myData.table5Data[i].cartridge_sn;
 								 csnList.push(csnVal);
 						var psnVal = myData.table5Data[i].printer_sn;
 								 psnList.push(psnVal);
-								 console.log(psnList);
-
-						var table = document.getElementById("myTable");
+						
+						var table = document.getElementById("myTable");						
 						var row = table.insertRow(1);
 						var cell1 = row.insertCell(0);
 						var cell2 = row.insertCell(1);
 						var cell3 = row.insertCell(2);
-
+						var cell4 = row.insertCell(2);
+						
 							cell1.innerHTML = myData.table5Data[i].cartridge_sn;
-							cell2.innerHTML = myData.table5Data[i].printer_sn;
+							cell2.innerHTML = myData.table5Data[i].printer_sn;							
 							cell3.setAttribute("id","editBtn");
 							cell3.innerHTML = edtButton;
+							cell4.setAttribute("id","delBtn");
+							cell4.innerHTML = deleteBtn;
 							alert("Success");
 							break;
 					}
 				}
 			}
 			else
-			{
+			{	
 				for (j = 0; j < psnList.length; j++)
 				{
-					console.log(inputTwo);
-					console.log(psnList[j]);
-
 					if(inputTwo == psnList[j])
 					{
 						alert("Serial Number Already Exist!");
@@ -64,13 +119,12 @@ var editList = false;
 					{
 						checkList = false;
 					}
-
+					
 				}
 
-				console.log(checkList);
 				if(!checkList)
 				{
-					for (i = 0; i < myData.table5Data.length; i++)
+					for (i = 0; i < myData.table5Data.length; i++) 
 					{
 						if((inputOne == myData.table5Data[i].cartridge_sn) && (inputTwo == myData.table5Data[i].printer_sn))
 						{
@@ -78,17 +132,19 @@ var editList = false;
 									 csnList.push(csnVal);
 							var psnVal = myData.table5Data[i].printer_sn;
 									 psnList.push(psnVal);
-									 console.log(psnList);
-							var table = document.getElementById("myTable");
+							var table = document.getElementById("myTable");	
 							var row = table.insertRow(1);
 							var cell1 = row.insertCell(0);
 							var cell2 = row.insertCell(1);
 							var cell3 = row.insertCell(2);
-
+							var cell4 = row.insertCell(2);
+							
 								cell1.innerHTML = myData.table5Data[i].cartridge_sn;
 								cell2.innerHTML = myData.table5Data[i].printer_sn;
 								cell3.setAttribute("id","editBtn");
 								cell3.innerHTML = edtButton;
+								cell4.setAttribute("id","delBtn");
+								cell4.innerHTML = deleteBtn;
 								alert("Success");
 								break;
 						}
@@ -96,21 +152,58 @@ var editList = false;
 				}
 			}
 		}
+		
+	//table-data_time_check	
+
+		for (i = 0; i < myData.table5Data.length; i++) 
+		{
+			if((inputOne == myData.table5Data[i].cartridge_sn) && (inputTwo == myData.table5Data[i].printer_sn))
+			{
+				var table = document.getElementById("myTable2");
+				var row = table.insertRow(1);
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				var cell3 = row.insertCell(2);
+
+					cell1.innerHTML = myData.table5Data[i].cartridge_sn;
+					cell2.innerHTML = myData.table5Data[i].printer_sn;
+					cell3.innerHTML = today;
+					break;
+			}
+		}
+
 	}
+	
+//edit button
+	
 	var index = "";
 	$("body").on("click","#editBtn",function()
 	{
-		var p1 = $(this).prev().prev().text();
-		var p2 = $(this).prev().text();
+		var p1 = $(this).prev().prev().prev().text();
+		var p2 = $(this).prev().prev().text();
 		index = $(this).parent().index();
-
+		
 		$("#mdOne").val(p1);
 		$("#mdTwo").val(p2);
-		console.log(index + " = index ");
 		$('#myModal1').modal('show');
-
+		
 	});
+	
+//delete button
 
+	var index = "";
+	$("body").on("click","#delBtn",function()
+	{	
+		for (x = 0; x < psnList.length; x++) 
+		{	
+			psnList.splice( index, 1 );
+		}
+		index = $(this).parent().index();
+		document.getElementById("myTable").deleteRow(index);
+	});	
+
+//Update Button
+		
 	$("#Update").click(function()
 	{
 		var inputMod1 = $('#mdOne').val();
@@ -118,28 +211,28 @@ var editList = false;
 		var table = document.getElementById("myTable"), rIndex;
 		var edtButton = '<a id="editBtn">EDIT</a>'
 		var i;
-
-
+		
+			
 			for (x = 0; x < psnList.length; x++)
 			{
 				var inputMod1 = $('#mdOne').val();
 				var inputMod2 = $('#mdTwo').val();
-
+				
 				if(inputMod2 == psnList[x])
-                {
-                    alert("Printer Serial Number Already Exist!");
-                    editList = true;
-                    break;
-                }
-                else
-                {
-                    editList = false;
-                }
+					{
+						alert("Printer Serial Number Already Exist!");
+						editList = true;
+						break;
+					}
+					else
+					{
+						editList = false;
+					}
 			}
-
+		
 			if(!editList)
 			{
-				for (i = 0; i < myData.table5Data.length; i++)
+				for (i = 0; i < myData.table5Data.length; i++) 
 				{
 					if((inputMod1 == myData.table5Data[i].cartridge_sn) && (inputMod2 == myData.table5Data[i].printer_sn))
 					{
@@ -149,22 +242,23 @@ var editList = false;
 								 psnList.shift();
 						var psnVal = myData.table5Data[i].printer_sn;
 								 psnList.push(psnVal);
-								  console.log(psnList);
-
+								 
 						var rows = document.getElementById("myTable").rows;
 							for (var j = 1; j < rows.length; j++)
 							{
-                                rows[j].id = "myRows" + (j+1);
-
-                                var x = document.getElementById("myTable").rows[index+1].cells;
-                                  x[0].innerHTML = myData.table5Data[i].cartridge_sn;
-                                  x[1].innerHTML = myData.table5Data[i].printer_sn;
-                                  break;
+									rows[j].id = "myRows" + (j+1);
+										
+									var x = document.getElementById("myTable").rows[index].cells;
+									  x[0].innerHTML = myData.table5Data[i].cartridge_sn;
+									  x[1].innerHTML = myData.table5Data[i].printer_sn;	
+									  break;
 							}
-
-                        alert("Success");
+						
+						   
+						alert("Success");
 						$('#myModal1').modal('hide');
 					}
 				}
-            }
+
+			}
 	});
